5dee87dc0d6cabd4f16a7aadbb67bb4a42af18d1
caremi <Willy Nilly>
2025-08-23 17:54:51 +0400

    add autoloading and frontend controller file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php declare(strict_types=1); // public/index.php
-
 // Define application constants
 define('CAREMI_START', microtime(true));  // Application start time for performance tracking
 define('BASE_PATH', dirname(__DIR__));    // Base directory path
@@ -7,10 +6,12 @@ define('ROOT_PATH', dirname(__FILE__));   // Root directory path
 define('ROOT_DIR', dirname(__FILE__));
 
 
+// Include Composer autoload to load dependencies
+require_once BASE_PATH. '/vendor/autoload.php';
+
 // bootstrapping
 require BASE_PATH . '/bootstrap/app.php';
 require BASE_PATH . '/bootstrap/performance.php';
-
 
 // request received
 
