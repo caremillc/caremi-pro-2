41fac9cefaca96b45eaab4f040743c01b2633d80
caremi <Willy Nilly>
2025-08-23 18:51:15 +0400

    Add Http Kernel

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
-<?php declare(strict_types=1); // public/index.php
+<?php declare(strict_types=1); 
+use Careminate\Http\Kernel;
 use Careminate\Http\Requests\Request;
 use Careminate\Http\Responses\Response;
 
-define('CAREMI_START', microtime(true));
-define('BASE_PATH', dirname(__DIR__));
+define('CAREMI_START', microtime(true));  // Application start time for performance tracking
+define('BASE_PATH', dirname(__DIR__));    // Base directory path
+define('ROOT_PATH', dirname(__FILE__));   // Root directory path
 define('ROOT_DIR', dirname(__FILE__));
-define('ROOT_PATH', dirname(__FILE__));
 
 // Include Composer autoload to load dependencies
 require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -16,14 +17,18 @@ require BASE_PATH . '/bootstrap/performance.php';
 
 // request received
 $request = Request::createFromGlobals();
-// send response (string of content)
-$content = '<h1>Hello World from index page</h1>';
 
-$response = new Response(content: $content, status: 200, headers: []);
+// Initializes the application's kernel 
+$kernel = new Kernel();
+
+// send response (string of content)
+$response = $kernel->handle($request);
 
 $response->send();
 
-// perform some logic
-
 // send response (string of content)
 dd($response);
+
+// send response (string of content)
+// echo 'Hello World';
+
