1d9c2cccd6411f6c82807f287e3c7e26ce4b0cdc
caremi <Willy Nilly>
2025-08-29 19:24:47 +0400

    Add Twig Template

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,22 +1,16 @@
 <?php declare(strict_types=1);
 namespace App\Http\Controllers;
 
-use Twig\Environment;
 use App\Widget\Widget;
-use Careminate\Http\Controllers\AbstractController;
 use Careminate\Http\Responses\Response;
 
-
-class HomeController extends AbstractController
+class HomeController extends Controller
 {
-    public function __construct(private Widget $widget)
-    {
-    }
+    public function __construct(private Widget $widget){}
 
     public function index(): Response
     {
-        $content = "<h1>Hello {$this->widget->name}</h1>";
-
-        return new Response($content);
+        $title = "Home Page";
+        return view('home.html.twig', compact('title'));
     }
 }
--- config/container.php
+++ config/container.php
@@ -8,6 +8,10 @@ $container = new \League\Container\Container();
 // Enable auto-resolution of dependencies through reflection
 $container->delegate(new \League\Container\ReflectionContainer(true));
 
+// Load application routes from an external configuration file.
+# get routes
+$routes = Careminate\Routing\Route::getRoutes();
+
 #parameters
 
 #env parameters
@@ -15,13 +19,13 @@ $appEnv = env('APP_ENV', 'production'); // Default to 'production' if not set
 $appKey = env('APP_KEY'); // Default to 'production' if not set
 $appVersion = env('APP_VERSION');
 
+# twig template path
+$templatesPath = BASE_PATH . '/templates/views';
+
 $container->add('APP_ENV', new \League\Container\Argument\Literal\StringArgument($appEnv));
 $container->add('APP_KEY', new \League\Container\Argument\Literal\StringArgument($appKey));
 $container->add('APP_VERSION', new \League\Container\Argument\Literal\StringArgument($appVersion));
 
-// Load application routes from an external configuration file.
-# get routes
-$routes = Careminate\Routing\Route::getRoutes();
 
 # Services
 // Bind RouterInterface to Router implementation
@@ -35,6 +39,24 @@ $container->add(\Careminate\Http\Kernel::class)
           // Extend RouterInterface definition to inject routes
 $container->extend(Careminate\Routing\Contracts\RouterInterface::class)
           ->addMethodCall('setRoutes',[new League\Container\Argument\Literal\ArrayArgument($routes)]);
+
+          // Register the Twig FilesystemLoader as a shared (singleton) service.
+// It will use the provided $templatesPath as the base directory for template files.
+$container->addShared('filesystem-loader', \Twig\Loader\FilesystemLoader::class)
+          ->addArgument(new \League\Container\Argument\Literal\StringArgument($templatesPath));
+
+// Register the Twig Environment as a shared (singleton) instance
+// and inject the 'filesystem-loader' service into its constructor.
+$container->addShared('twig', \Twig\Environment::class)
+          ->addArgument('filesystem-loader');
+
+          // Register the AbstractController so it can be resolved by the container.
+$container->add(\Careminate\Http\Controllers\AbstractController::class);
+
+// Automatically call the setContainer() method on any class that extends AbstractController
+// This injects the container itself into the controller, enabling dependency resolution within controllers.
+$container->inflector(\Careminate\Http\Controllers\AbstractController::class)
+          ->invokeMethod('setContainer', [$container]);
 
 // dd($container);
 return $container;
