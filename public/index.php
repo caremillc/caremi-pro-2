60c1011bb752134ca973ba0bfb2c34b45319f61b
caremi <Willy Nilly>
2025-09-19 20:11:47 +0400

    Adding Routes

--- public/index.php
+++ public/index.php
@@ -1,37 +1,31 @@
 <?php declare(strict_types=1); 
 
 use Careminate\Http\Kernel;
+use Careminate\Routing\Router;
 use Careminate\Http\Requests\Request;
-use Careminate\Http\Responses\Response;
 
 // ---------------------------------------------------------
 // Bootstrap the framework
 // ---------------------------------------------------------
 require __DIR__ . '/../bootstrap/app.php';
 
-// ---------------------------------------------------------
-// Handle the request (placeholder logic for now)
-// ---------------------------------------------------------
+require route_path('web.php');
+
 // request received
 $request = Request::createFromGlobals();
 
-// ---------------------------------------------------------
-// Handle the request
-// ---------------------------------------------------------
-$request = Request::createFromGlobals();
+//instantiate router
+$router = new Router();
 
 // Initializes the application's kernel 
-$kernel = new Kernel();
+$kernel = new Kernel($router);
 
-// Generate response
+// send response (string of content)
 $response = $kernel->handle($request);
 
-
-// For debugging (optional, remove in production)
-// dd($kernel, $request);
-
-// Send the response to client
 $response->send();
+
+// dd($response);
 
 // ---------------------------------------------------------
 // Log performance after response
